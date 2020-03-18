232875a866c65261fbd43e9a47ec30355acdc2ea
MChiumya <michaelchiumya@yahoo.co.uk>
2020-03-18 20:37:18 +0000

    homepage done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MenuComponent } from './component/menu/menu.component';
+import { HomeComponent } from './component/home/home.component';
+import { ArtistComponent } from './component/artist/artist.component';
+import { RoutingModule } from './routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    HomeComponent,
+    ArtistComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/routing.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { RouterModule,Routes } from '@angular/router';
+import { HomeComponent } from './component/home/home.component';
+import { ArtistComponent } from './component/artist/artist.component';
+
+export const routes: Routes = [
+  {path:'home', component: HomeComponent},
+  {path:'artist', component: ArtistComponent}
+];
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    [RouterModule.forRoot(routes)]
+  ],
+  exports:[RouterModule]
+})
+export class RoutingModule { }
